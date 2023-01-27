14d3b7b65aaa98cac1d0d85dfdff8da28eb6bddd
HuanPT <104756429+HuanPT@users.noreply.github.com>
2023-01-27 18:39:45 +0700

    Update home.js

--- theMovies/src/assets/js/home.js
+++ theMovies/src/assets/js/home.js
@@ -14,6 +14,190 @@ const main = document.querySelector(".main");
 const mainContainer = main.querySelector(".container");
 const mainRow = mainContainer.querySelector(".row");
 
+const makeHeader = () => {
+  header.innerHTML += `
+    <div class="container">
+      <div class="row">
+
+        <div class="col-12 d-md-none heade__Mobile">
+          <div class="header__top-wrap">
+            <div class="header__search">
+
+              <div class="dropdown">
+                <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <form class="search__form">
+                      <label class="input-tag">
+                          <input type="text" id="search__mobile">
+                          <span class="place">
+                              Tìm kiếm
+                          </span>
+                      </label>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="header__top-logo">
+              <a href="/"><img src="./assets/img/paner.png" alt="logo"></a>
+            </div>
+
+            <div class="header__navbars">
+              <div class="btn__navbars">
+                <i class="fa-solid fa-bars"></i>
+              </div>
+              <div class="navbars__mobile-detail">
+                <div class="overlay"></div>
+                <div class="navbars__mobile-wrap">
+                  <div class="navbars__mobile-header">
+                    <div class="mobile__user-name">
+                      <h3>Chào HuanPT!</h3>
+                      <div class="user__name-avatar">
+                        <i class="fa-solid fa-user"></i>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="navbars__mobile-main">
+                    <ul class="navbars__mobile-list">
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          <i class="fa-solid fa-circle-info"></i>
+                          Tài khoản
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          <i class="fa-solid fa-heart"></i>
+                          Phim yêu thích
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          <i class="fa-solid fa-bookmark"></i>
+                          Phim đã lưu
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          <i class="fa-solid fa-clock-rotate-left"></i>
+                          Lịch sử xem</a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                          Đăng xuất
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          Phim hot
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          Phim lẻ
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          Phim bộ
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="user-menu-item">
+                          Phim mới
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 d-none d-md-block header__desktop">
+          <div class="header__top-wrap">
+
+
+            <div class="header__top-logo">
+              <a href="/"><img src="./assets/img/paner.png" alt="logo"></a>
+            </div>
+
+            <div class="header__center d-flex">
+              <ul class="header__center-menu d-flex">
+                <li class="center-menu-item">
+                  <a href="#">Phim hot</a>
+                </li>
+                <li class="center-menu-item">
+                  <a href="#">Phim lẻ</a>
+                </li>
+                <li class="center-menu-item">
+                  <a href="#">Phim bộ</a>
+                </li>
+                <li class="center-menu-item">
+                  <a href="#">Phim mới</a>
+                </li>
+              </ul>
+
+              <div class="header__center-search">
+                <form class="search__form">
+                    <label class="input-tag">
+                        <input type="text" id="search__desktop">
+                        <span class="place">
+                            Tìm kiếm
+                        </span>
+                    </label>
+                </form>
+              </div>
+            </div>
+
+            <div class="header__nav-user d-flex">
+              <i class="user-icon fa-solid fa-user"></i>
+
+              <div class="header__nav-wrap">
+                <div class="triangle"></div>
+                <ul class="nav-user-menu">
+                  <li>
+                    <a class="user-menu-item" href="/account.html"><i class="fa-solid fa-circle-info"></i>
+                      Tài khoản
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/account.html" class="user-menu-item">
+                      <i class="fa-solid fa-heart"></i>
+                      Phim yêu thích
+                    </a>
+                  </li>
+                  <li>
+                    <a class="user-menu-item" href="/account.html"><i class="fa-solid fa-bookmark"></i>
+                      Phim đã lưu
+                    </a>
+                  </li>
+                  <li>
+                    <a class="user-menu-item" href="/account.html"><i class="fa-solid fa-clock-rotate-left"></i>
+                      Lịch sử xem
+                    </a>
+                  </li>
+                  <li>
+                    <a class="user-menu-item" href="/account.html"><i class="fa-solid fa-right-from-bracket"></i>
+                      Đăng xuất
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+};
+
 fetch(
   api.genresList +
     new URLSearchParams({
@@ -26,13 +210,11 @@ fetch(
     console.log(data.genres);
     data.genres.forEach((item, i) => {
       // console.log(item, i);
-      if (i < 11) {
-        if (i !== 5) {
-          if (i % 2 == 0) {
-            fetchMoviesListByGenres(item.id, item.name);
-          } else {
-            fetchMoviesListByCardBig(item.id, item.name);
-          }
+      if (i < 11 && i !== 5) {
+        if (i % 2 == 0) {
+          fetchMoviesListByGenres(item.id, item.name);
+        } else {
+          fetchMoviesListByCardBig(item.id, item.name);
         }
       }
     });
@@ -211,6 +393,7 @@ const makeCardsBig = (id, data) => {
 };
 
 window.onload = () => {
+  // makeHeader();
   navSearchMobile();
   navSearchDesktop();
   navMobile();
